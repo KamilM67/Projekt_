e895d56e5b5f8207098e1f89d97c78bc7c0d45a5
wiktorm67 <wiktor.matysiak2115@gmail.com>
2026-06-12 01:05:36 +0200

    Milestone 5 Architektura GameManager

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,3 +1,40 @@
 #include "GameManager.h"
 
-GameManager::GameManager() {}
+// domyslny konstruktor przypisujacy na start latwy poziom trudnosci
+GameManager::GameManager() { currentDifficulty = Difficulty::EASY; }
+
+// pusty destruktor czyszczacy obiekt menedzera gry
+GameManager::~GameManager() {}
+
+// metoda konfigurujaca parametry zycia i zasobow poczatkowych zaleznie od trudnosci
+void GameManager::setupDifficulty(Difficulty diff) {}
+
+// resetuje wszystkie zmienne, listy przeciwnikow i wiez wracajac do stanu poczatkowego
+void GameManager::restartGame() {}
+
+// weryfikuje czy klikniete miejsce (x,y) jest wolnym polem na wieze i jesli tak probuje dokonac zakupu
+bool GameManager::placeTower(float x, float y) { return false; }
+
+// inicjalizuje elementy panelu uzytkownika i przypisuje im odpowiednie ksztalty, kolory i teksty
+void GameManager::initHUD() {}
+
+// generuje wizualna reprezentacje przerywanej lub ciaglej trasy, ktora beda maszerowac balony
+void GameManager::initPathGraphics() {}
+
+// przygotowuje kwadratowe pola poza glowna sciezka, podswietlajace sie jako miejsce na wieze
+void GameManager::initPlacementSlots() {}
+
+// dba o to, by teksty w hud np. liczba gotowki i pozostale zycia zawsze wskazywaly prawdziwa wartosc
+void GameManager::updateUI() {}
+
+// najwazniejsza funkcja z glowna petla gry odswiezana co klatke - oblicza logike i kolizje
+void GameManager::update(float dt) {}
+
+// zarzadza kolejnoscia renderowania by np. interfejs rysowal sie zawsze najwyzej a mapa pod spodem
+void GameManager::draw(sf::RenderWindow& window) {}
+
+// funkcja tworzaca na wejsciu sciezki nowego wroga o zadanych parametrach zaleznych od fali
+void GameManager::spawnEnemy() {}
+
+// po zakonczeniu gry czyta plik z rekordami, aktualizuje jego zawartosc i tworzy sformatowany napis koncowy
+void GameManager::loadAndSaveLeaderboard(int finalScore) {}
--- GameManager.h
+++ GameManager.h
@@ -1,10 +1,70 @@
+// zabezpieczenie przed wielokrotnym wczytaniem pliku naglowkowego
 #ifndef GAMEMANAGER_H
 #define GAMEMANAGER_H
 
-class GameManager
-{
-public:
-    GameManager();
-};
+// zabezpieczenie definicji typu wyliczeniowego
+#ifndef DIFFICULTY_ENUM_GUARD
+#define DIFFICULTY_ENUM_GUARD
+// dostepne poziomy trudnosci: latwy, sredni, trudny
+enum class Difficulty { EASY, MEDIUM, HARD };
+#endif
 
-#endif // GAMEMANAGER_H
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <memory>
+#include <string>
+#include "GameObject.h"
+#include "Tower.h"
+#include "Enemy.h"
+#include "Projectile.h"
+#include "Menu.h"
+#include "Config.h"
+
+// struktura przechowujaca wynik do rankingu (liczba punktow, status gry, poziom trudnosci)
+struct ScoreEntry { int score; std::string outcome; std::string difficulty; };
+// struktura reprezentujaca miejsce na mapie z mozliwoscia budowy wiezy (jej ksztalt i czy juz jest zajete)
+struct PlacementSlot { sf::RectangleShape shape; bool isOccupied; };
+// struktura dla przyciskow w dolnym panelu pozwalajacych na zakup danej wiezy
+struct HudButton { TowerType type; int price; sf::CircleShape icon; sf::Text stats; };
+
+// glowna klasa odpowiedzialna za przebieg rozgrywki
+class GameManager {
+private:
+    // czcionka i aktualnie wybrany poziom trudnosci gry
+    sf::Font font; Difficulty currentDifficulty;
+    // wektor przechowujacy wszystkie aktywne obiekty na mapie (wrogowie, pociski)
+    std::vector<std::unique_ptr<GameObject>> gameObjects;
+    // miejsca, w ktorych gracz moze wybudowac swoja obrone
+    std::vector<PlacementSlot> placementSlots;
+    // lista punktow wyznaczajacych droge dla przeciwnikow oraz ich graficzna reprezentacja
+    std::vector<sf::Vector2f> mapPath; std::vector<sf::RectangleShape> pathSegments;
+    // graficzne tla dla calej mapy i glownej bazy gracza na koncu sciezki
+    sf::RectangleShape backgroundRect; sf::RectangleShape baseShape;
+    // tlo interfejsu (hud) oraz lista przyciskow do wybierania i kupowania wiez
+    sf::RectangleShape hudBackground; std::vector<HudButton> hudButtons;
+    // obiekty tekstowe i prostokaty wyswietlajace statystyki (np. monety, zapowiedzi fal)
+    sf::Text moneyText; sf::RectangleShape statsBackground; sf::Text waveAnnouncementText;
+    // przycisk powrotu do menu oraz teksty informujace o przegranej lub wygranej
+    sf::RectangleShape hudMenuButton; sf::Text hudMenuText; sf::Text gameOverText; sf::Text winText;
+    // elementy wyswietlane po zakonczeniu gry (wynik, przycisk restartu i powrotu do menu)
+    sf::Text scoreText; sf::RectangleShape restartButton; sf::Text restartText; sf::RectangleShape menuButton; sf::Text menuText;
+    // statystyki obecnej partii: pieniadze, ilosc zyc, numery fal, limity wrogow i bossow
+    int money; int lives; int currentWave; int maxWaves; int enemiesSpawnedInWave; int bossesToSpawn;
+    // timery sterujace czestotliwoscia pojawiania sie wrogow i dlugoscia przerw
+    float spawnTimer; float wavePauseTimer; float bossSpawnCooldown;
+    // flagi sprawdzajace obecny stan meczu (pauza, koniec gry, zwyciestwo, czy zapisano wynik)
+    bool isWavePause; bool isGameOver; bool isWin; bool scoreSaved;
+    // lista uzywana do trzymania najlepszych wynikow z pliku oraz wybrany aktualnie w hud typ wiezy
+    std::vector<ScoreEntry> highScores; sf::Text leaderboardText; TowerType selectedType;
+    // paleta kolorow wykorzystywanych do rysowania sciezki, trawy i interfejsu uzytkownika
+    sf::Color color_Ground, color_Path, color_SlotFill, color_SlotOutline, color_HudBg, color_StatsBgFill, color_StatsBgOutline;
+
+    // prywatne metody przygotowujace poszczegolne wizualne elementy mapy, intefejs i ranking
+    void initHUD(); void initPathGraphics(); void initPlacementSlots(); void updateUI(); void spawnEnemy(); void loadAndSaveLeaderboard(int finalScore);
+public:
+    // konstruktor i destruktor klasy zarzadcy
+    GameManager(); ~GameManager();
+    // metody dostepne na zewnatrz do resetowania stanu, wyboru poziomu, aktualizacji logiki i renderowania obrazu
+    void restartGame(); void setupDifficulty(Difficulty diff); void update(float dt); void draw(sf::RenderWindow& window); bool placeTower(float x, float y);
+};
+#endif
