fb9bc643820ce547174e0b73ff822479d4d2d558
KamilM67 <kamil.menes50@gmail.com>
2026-06-11 22:42:21 +0200

    mil1

--- Config.h
+++ Config.h
@@ -1,4 +1,32 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#endif // CONFIG_H
+// dolaczenie biblioteki sfml odpowiedzialnej za grafike
+#include <SFML/Graphics.hpp>
+
+// struktura przechowujaca globalne ustawienia i stale konfiguracji gry
+struct Config {
+    // wymiary okna gry w pikselach
+    static const int WINDOW_WIDTH = 1280;
+    static const int WINDOW_HEIGHT = 720;
+
+    // tytul wyswietlany na gornej belce okna gry
+    static constexpr const char* WINDOW_TITLE = "Tower Defense Projekt";
+
+    // statystyki startowe dla gracza
+    static const int STARTING_MONEY = 200; // poczatkowy stan konta
+    static const int STARTING_LIVES = 10;  // poczatkowa liczba zyc
+
+    // ekonomia gry i balanse obrony
+    static const int TOWER_COST = 50;      // koszt postawienia jednej wiezy
+
+    // ustawienia wygladu elementow gry za pomoca sfml
+    static inline const sf::Color TOWER_COLOR = sf::Color::Blue;        // kolor wiezy
+    static inline const sf::Color PROJECTILE_COLOR = sf::Color::Yellow; // kolor pocisku
+
+    // konfiguracja fal przeciwnikow
+    static const int BASE_ENEMIES_PER_WAVE = 10;       // liczba przeciwnikow w pierwszej fali
+    static const int ENEMIES_INCREMENT_PER_WAVE = 5;   // o ile wiecej wrogow pojawi sie w kazdej kolejnej fali
+};
+
+#endif
--- GameObject.h
+++ GameObject.h
@@ -1,4 +1,20 @@
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
-#endif // GAMEOBJECT_H
+// dolaczenie biblioteki sfml potrzebnej do operacji graficznych
+#include <SFML/Graphics.hpp>
+
+// klasa abstrakcyjna bedaca baza dla wszystkich obiektow w grze (np. wieze, wrogowie)
+class GameObject {
+public:
+    // wirtualny destruktor zapewniaacy poprawne czyszczenie pamieci klas pochodnych
+    virtual ~GameObject() = default;
+
+    // czysta funkcja wirtualna do aktualizacji logiki obiektu, dt to czas od ostatniej klatki (delta time)
+    virtual void update(float dt) = 0;
+
+    // czysta funkcja wirtualna odpowiedzialna za rysowanie obiektu w oknie gry
+    virtual void draw(sf::RenderWindow& window) = 0;
+};
+
+#endif
